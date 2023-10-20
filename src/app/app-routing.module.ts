077f2b5dd7fa015ab878954c85c4f34d95b8689f
AkashpatilOA <akashpatil@opticalarc.com>
2023-10-20 14:51:19 +0530

    Static Registration page added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserAuthenticationModule } from './user-authentication/user-authentication.module';
 
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes) , UserAuthenticationModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
