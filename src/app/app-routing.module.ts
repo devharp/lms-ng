2b678459cf011e0b2bfa3943c45c0ac376312cab
devharp <hsingh.161297@gmail.com>
2023-10-20 15:16:09 +0530

    fixed routing; fixed tailwind css

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UserAuthenticationModule } from './user-authentication/user-authentication.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'auth', loadChildren: () => import('./user-authentication/user-authentication.module').then(e => e.UserAuthenticationModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes) , UserAuthenticationModule],
